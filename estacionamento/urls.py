44b2f47224c2d275a58cab04b60e2e3712181ba5
Felipe Gomes <felipe.gomes@SUDEMAPB.INTRA>
2026-02-25 09:34:06 -0300

    Corrigindo erro 404: Redirecionando raiz para a tela de cadastro

--- estacionamento/urls.py
+++ estacionamento/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from django.views.generic import RedirectView 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 2. Adicione esta linha: Ela pega a raiz do site ('') e joga para o cadastro
+    path('', RedirectView.as_view(url='/cadastrar_cliente/', permanent=True)), 
+    
+    # 3. Mantém as urls do seu app funcionando normalmente
     path('', include('cadastro.urls')), 
 ]
